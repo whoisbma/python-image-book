87915ab0a5e9dc1d739b67355938b1d611a3b2f0
Bryan Ma <bryanhma@gmail.com>
2016-12-14 09:44:53 -0500

    utils

---
+++ utils/imtools.py
@@ -0,0 +1,5 @@
+import os
+
+def get_imlist(path):
+		""" Returns a list of filenames for all jpg images in a directory. """
+		return [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.jpeg')]
