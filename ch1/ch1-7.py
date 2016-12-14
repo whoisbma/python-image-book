c2143da62472ab09f1e86185a29b27ceb54b6523
Bryan Ma <bryanhma@gmail.com>
2016-12-14 16:10:52 -0500

    7

--- ch1/ch1-7.py
+++ ch1/ch1-7.py
@@ -16,5 +16,22 @@ im3 = (100.0/255) * im + 100 		# clamp to interval 100...200
 im4 = 255.0 * (im/255.0) ** 2		# squared (lower the darkness values)
 
 # check the minimum and maximum values of each image using:
-print int(in.min()), int(im.max())
+print int(im.min()), int(im.max())
 
+figure()
+imshow(im)
+
+print int(im2.min()), int(im2.max())
+
+figure()
+imshow(im2)
+
+print int(im3.min()), int(im3.max())
+
+figure()
+imshow(im3)
+
+print int(im4.min()), int(im4.max())
+
+figure()
+imshow(im4)
