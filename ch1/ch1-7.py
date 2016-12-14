41a8606797cc4161d3cd05853476fb864d517623
Bryan Ma <bryanhma@gmail.com>
2016-12-14 16:24:13 -0500

    7 and resize util

--- ch1/ch1-7.py
+++ ch1/ch1-7.py
@@ -5,9 +5,10 @@
 # take any function f that maps the interval 0...255 to itself
 
 from PIL import Image
-from numpy import *
+from pylab import *
 
-im = array(Image.open('pug.jpg').convert('L'))
+im = array(Image.open('images/pug.jpg').convert('L'))
+gray()
 
 im2 = 255 - im 						# invert image
 
@@ -18,7 +19,6 @@ im4 = 255.0 * (im/255.0) ** 2		# squared (lower the darkness values)
 # check the minimum and maximum values of each image using:
 print int(im.min()), int(im.max())
 
-figure()
 imshow(im)
 
 print int(im2.min()), int(im2.max())
@@ -35,3 +35,11 @@ print int(im4.min()), int(im4.max())
 
 figure()
 imshow(im4)
+
+show()
+
+# the reverse of the array() transformation can be done using the PIL function fromarray()
+pil_im = Image.fromarray(im)
+
+# if you did some operation to change the type from uint8 to another data type such as im3 or im4 in the example above, you need to convert back before creating the PIL image:
+pil_im = Image.fromarray(uint8(im))
--- utils/imtools.py
+++ utils/imtools.py
@@ -1,5 +1,14 @@
 import os
 
 def get_imlist(path, file_ext):
-		""" Returns a list of filenames for all jpg images in a directory. """
-		return [os.path.join(path, f) for f in os.listdir(path) if f.endswith(file_ext)]
+	""" Returns a list of filenames for all jpg images in a directory. """
+	return [os.path.join(path, f) for f in os.listdir(path) if f.endswith(file_ext)]
+
+
+
+# in order to resize an image with numpy arrays:
+# use the PIL object conversion to make a simple image resizing fucntion.
+def imresize(im,sz):
+	""" Resize an image array using PIL. """
+	pil_im = Image.fromarray(uint8(im))
+	return array(pil_im.resize(sz))
