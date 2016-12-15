371309b7e53a04aed499097b2a1d55f083e6ce78
Bryan Ma <bryanhma@gmail.com>
2016-12-15 11:14:44 -0500

    rename

---
+++ ch1/ch1-14-exercises.py
@@ -0,0 +1,9 @@
+# ch1-14-exercises.py
+# pp. 26
+
+from PIL import Image
+import numpy as numpy
+import pylab as pylab
+
+# 1: Take an image and apply Gaussian blur. Plot the image contours for increasing blur values.
+
