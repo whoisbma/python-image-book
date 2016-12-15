285ad6c1a01db3f11bb48323f161050518c75821
Bryan Ma <bryanhma@gmail.com>
2016-12-14 19:00:52 -0500

    10

--- ch1/ch1-10-scipy.py
+++ ch1/ch1-10-scipy.py
@@ -23,4 +23,47 @@ im2 = uint8(im2)
 # also could have used im2 = array(im2, 'uint8')
 
 imshow(im2)
+
+# Image Derivatives
+
+# How the image intensity changes over the image is important information and is used for many applications.
+
+# applying derivative filters using the standard convolution available in the scipy.ndimage.filters module.
+
+im3 = array(Image.open('data/empire.jpg').convert('L'))
+
+# sobol derivative filters
+imx = zeros(im3.shape)
+filters.sobel(im3,1,imx)
+
+imy = zeros(im3.shape)
+filters.sobel(im3,0,imy)
+
+magnitude = sqrt(imx**2+imy**2)
+
+gray()
+
+figure()
+imshow(imx)
+
+figure()
+imshow(imy)
+
+figure()
+imshow(magnitude)
+
+# Using this approach has the drawback that derivatives are taken on the scale determined by the image resolution. To be more robust to image noise and to compute derivatives at any scale, Gaussian Derivative Filters can be used.
+
+# The filters.gaussian_filter() function we used for blurring earlier
+sigma = 5 # standard deviation
+imx2 = zeros(im3.shape)
+filters.gaussian_filter(im3, (sigma,sigma), (0,1), imx2)
+imy2 = zeros(im3.shape)
+filters.gaussian_filter(im3, (sigma,sigma), (1,0), imy2)
+
+figure()
+imshow(imx2)
+figure()
+imshow(imy2)
+
 show()
